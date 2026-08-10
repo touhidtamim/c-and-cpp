b0558ac8f3a20f1852e893e0e755ea83d441b480
Touhid Tamim <194215315+touhidtamim@users.noreply.github.com>
2026-08-10 23:08:51 +0600

    solved simple if else problem

---
+++ if_else/divisibleBy3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+int n;
+scanf("%d", &n);
+
+if(n % 3 == 0){
+  printf("The number is divisible by 3");
+} else{
+  printf("The number is not divisible by 3");
+}
+
+  return 0;
+}
