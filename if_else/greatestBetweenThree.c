753fdf436a4cb77fc1160f7ec20506ecb5b53c3f
Touhid Tamim <194215315+touhidtamim@users.noreply.github.com>
2026-08-16 23:47:34 +0600

    solved greated between three integer

---
+++ if_else/greatestBetweenThree.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b) {
+        if (a >= c) {
+            printf("%d is the greatest\n", a);
+        } else {
+            printf("%d is the greatest\n", c);
+        }
+    } else {
+        if (b >= c) {
+            printf("%d is the greatest\n", b);
+        } else {
+            printf("%d is the greatest\n", c);
+        }
+    }
+
+    return 0;
+}
